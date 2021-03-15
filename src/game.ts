9c4d92545827f8491a9f8c93b65bc16d47e06137
Abby <abby@mythril.co>
2021-03-15 03:00:48 +0000

    add icons and basic drag

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,67 @@
-import 'phaser';
+import "phaser";
+import Loading from "./scenes/loading";
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
-    }
+export default class Main extends Phaser.Scene {
+  g: Phaser.GameObjects.Graphics;
+  icon: Phaser.GameObjects.Sprite;
+  warn: Phaser.GameObjects.Sprite;
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
-    }
+  constructor() {
+    super("main");
+  }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+  preload() {
+    console.log("preload");
+    // this.load.image("logo", "assets/phaser3-logo.png");
+    // this.load.image("libs", "assets/libs.png");
+    // this.load.glsl("bundle", "assets/plasma-bundle.glsl.js");
+    // this.load.glsl("stars", "assets/starfields.glsl.js");
+  }
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+  create() {
+    this.g = this.add.graphics();
 
-        this.add.image(400, 300, 'libs');
+    this.icon = this.add.sprite(200, 200, "database");
+    this.icon.setInteractive();
+    this.input.setDraggable(this.icon);
 
-        const logo = this.add.image(400, 70, 'logo');
+    this.warn = this.add.sprite(200, 200, "server_web");
+    this.warn.setInteractive();
+    this.input.setDraggable(this.warn);
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
-    }
+    this.input.on("drag", (pointer, gob, dragX, dragY) => {
+      gob.x = dragX;
+      gob.y = dragY;
+    });
+
+    // this.input.on("drag", (pointer, gob, dragX, dragY) => {
+    //   gob.x = dragX;
+    //   gob.y = dragY;
+    // });
+
+    // this.tweens.add({
+    //   targets: database,
+    //   y: 350,
+    //   duration: 1500,
+    //   ease: "Sine.inOut",
+    //   yoyo: true,
+    //   repeat: -1,
+    // });
+  }
+
+  update() {
+    this.g.clear();
+    this.g.lineStyle(12, 0xffffff, 1);
+    this.g.lineBetween(this.icon.x, this.icon.y, this.warn.x, this.warn.y);
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+  type: Phaser.AUTO,
+  backgroundColor: "#125555",
+  width: 800,
+  height: 600,
+  scene: [Loading, Main],
 };
 
 const game = new Phaser.Game(config);
---
+++ src/scenes/loading.ts
@@ -0,0 +1,80 @@
+import "phaser";
+import iconList from "../icon-list.js";
+
+export default class Loading extends Phaser.Scene {
+  constructor() {
+    super("loading");
+  }
+
+  preload() {
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    const { width, height } = this.cameras.main;
+    const style = { font: "18px monospace", color: "#ffffff" };
+
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: "loading..",
+      style,
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: `0%`,
+      style,
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    const assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: "",
+      style,
+    });
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on("progress", (value) => {
+      percentText.setText(`${Math.floor(value * 100)}%`);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    this.load.on("filecomplete", (file) => {
+      assetText.setText(`loading: ${file}`);
+    });
+
+    this.load.on("complete", () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+
+      setTimeout(() => {
+        this.scene.start("main");
+      }, 1000);
+    });
+
+    this.load.image("logo", "assets/phaser3-logo.png");
+    this.load.image("libs", "assets/libs.png");
+    this.load.glsl("bundle", "assets/plasma-bundle.glsl.js");
+    this.load.glsl("stars", "assets/starfields.glsl.js");
+
+    const svgSizeConfig = { width: 120, height: 120 };
+    for (const icon of iconList) {
+      console.log(icon.name);
+      this.load.svg(icon.name, `assets/icons/${icon.filename}`, svgSizeConfig);
+    }
+  }
+
+  create() {
+    const logo = this.add.image(400, 300, "logo");
+  }
+}
